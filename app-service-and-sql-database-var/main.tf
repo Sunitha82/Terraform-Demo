86d9a2032c594012d827f222c09fabad2f44aeca
Sunitha82 <56600171+Sunitha82@users.noreply.github.com>
2023-03-22 16:07:08 +0000

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_app_service_plan" "ASP-TerraForm" {
   name                = "terraform-appserviceplan"
   location            = azurerm_resource_group.RG-Terraform.location
   resource_group_name = azurerm_resource_group.RG-Terraform.name
+  kind = "Windows"
 
   sku {
     tier = "Standard"
@@ -21,7 +22,7 @@ resource "azurerm_app_service" "AS-Terraform" {
   app_service_plan_id = azurerm_app_service_plan.ASP-TerraForm.id
 
   site_config {
-    dotnet_framework_version = "v4.0"
+    dotnet_framework_version = "v6.0"
     scm_type                 = "LocalGit"
   }
 
